792950ff86678a0d0f384668632263de7fa9bb24
Eshana Milan Weerasinghe <50627267+EshanaWeerasinghe@users.noreply.github.com>
2023-10-17 15:47:56 +0530

    #http get endpoint

--- LibraryAPI.Test/BooksControllerTest.cs
+++ LibraryAPI.Test/BooksControllerTest.cs
@@ -2,36 +2,61 @@ using Library.API.Controllers;
 using Library.API.Data.Models;
 using Library.API.Data.Services;
 using Microsoft.AspNetCore.Mvc;
+using Xunit;
 
 namespace LibraryAPI.Test
 {
     public class BooksControllerTest
     {
-       BooksController _controller;
-       IBookService _service;
+        private BooksController _controller;
+        private IBookService _service;
 
         public BooksControllerTest()
         {
-            _service = new BookService();
-            _controller = new BooksController( _service );
+            _service = new BookService(); // You should have a BookService class implementing IBookService.
+            _controller = new BooksController(_service);
         }
 
         [Fact]
         public void GetAllTest()
         {
-            //arrange
-            //act
+            // Arrange
+
+            // Act
             var result = _controller.Get();
-            //assert
+
+            // Assert
             Assert.IsType<OkObjectResult>(result.Result);
 
-            var list = result.Result as OkObjectResult;
-            Assert.IsType<List<Book>>(list.Value);
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsType<List<Book>>(okResult.Value);
 
-            var listBooks = list.Value as List<Book>;
+            var listBooks = okResult.Value as List<Book>;
             Assert.Equal(5, listBooks.Count);
         }
 
+        [Theory]
+        [InlineData("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200", "ab2bd817-98cd-4cf3-a80a-53ea0cd9c111")]
+        public void GetBookByIdTest(string guid1, string guid2)
+        {
+            // Arrange
+            var validGuid = new Guid(guid1);
+            var invalidGuid = new Guid(guid2);
 
+            // Act
+            var notFoundResult = _controller.Get(invalidGuid);
+            var okResult = _controller.Get(validGuid);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(notFoundResult.Result);
+            Assert.IsType<OkObjectResult>(okResult.Result);
+
+            var item = okResult.Result as OkObjectResult;
+            Assert.IsType<Book>(item.Value);
+
+            var bookItem = item.Value as Book;
+            Assert.Equal(validGuid, bookItem.Id);
+            Assert.Equal("Managing Oneself", bookItem.Title);
+        }
     }
 }
